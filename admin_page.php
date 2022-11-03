d9f6257d163ef1d84e46df917dbb84eba882c7e9
camilotorres <camivad@gmail.com>
2022-11-03 16:36:43 -0500

    primer envio de archivos

---
+++ admin_page.php
@@ -0,0 +1,164 @@
+<?php
+
+@include 'config.php';
+
+if(isset($_POST['add_product'])){
+
+   $product_name = $_POST['product_name'];
+   $product_price = $_POST['product_price'];
+   $product_image = $_FILES['product_image']['name'];
+   $product_image_tmp_name = $_FILES['product_image']['tmp_name'];
+   $product_image_folder = 'uploaded_img/'.$product_image;
+
+   if(empty($product_name) || empty($product_price) || empty($product_image)){
+      $message[] = 'please fill out all';
+   }else{
+      $insert = "INSERT INTO products(name, price, image) VALUES('$product_name', '$product_price', '$product_image')";
+      $upload = mysqli_query($conn,$insert);
+      if($upload){
+         move_uploaded_file($product_image_tmp_name, $product_image_folder);
+         $message[] = 'new product added successfully';
+      }else{
+         $message[] = 'could not add the product';
+      }
+   }
+
+};
+
+if(isset($_GET['delete'])){
+   $id = $_GET['delete'];
+   mysqli_query($conn, "DELETE FROM products WHERE id = $id");
+   header('location:admin_page.php');
+};
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+   <title>admin page</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<!-- Navbar  -->
+<header>
+		<div class="menu_bar">
+			<a href="#" class="bt-menu"><span class="icon-list2"></span>Menú</a>
+		</div>
+
+		<nav>
+			<ul>
+				<li><a href="#"><span class="icon-house"></span>Inicio</a></li>
+				<li><a href="#"><span class="icon-suitcase"></span>Registro</a></li>
+				<li class="submenu">
+					<a href="#"><span class="icon-rocket"></span>Pedidos<span class="caret icon-arrow-down6"></span></a>
+					<ul class="children">
+						<li><a href="#">SubElemento #1 <span class="icon-dot"></span></a></li>
+						<li><a href="#">SubElemento #2 <span class="icon-dot"></span></a></li>
+						<li><a href="#">SubElemento #3 <span class="icon-dot"></span></a></li>
+					</ul>
+				</li>
+				<li><a href="#"><span class="icon-earth"></span>Servicios</a></li>
+				<li><a href="#"><span class="icon-mail"></span>Contacto</a></li>
+			</ul>
+		</nav>
+	</header>
+
+<body>
+
+<script>
+    $(document).ready(main);
+
+var contador = 1;
+
+function main () {
+	$('.menu_bar').click(function(){
+		if (contador == 1) {
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+	});
+
+	// Mostramos y ocultamos submenus
+	$('.submenu').click(function(){
+		$(this).children('.children').slideToggle();
+	});
+}
+</script>
+
+<?php
+
+if(isset($message)){
+   foreach($message as $message){
+      echo '<span class="message">'.$message.'</span>';
+   }
+}
+
+?>
+   
+<div class="container">
+
+   <div class="admin-product-form-container">
+
+      <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
+         <h3>add a new product</h3>
+         <input type="text" placeholder="enter product name" name="product_name" class="box">
+         <input type="number" placeholder="enter product price" name="product_price" class="box">
+         <input type="file" accept="image/png, image/jpeg, image/jpg" name="product_image" class="box">
+         <input type="submit" class="btn" name="add_product" value="add product">
+      </form>
+
+   </div>
+
+   <?php
+
+   $select = mysqli_query($conn, "SELECT * FROM products");
+   
+   ?>
+   <div class="product-display">
+      <table class="product-display-table">
+         <thead>
+         <tr>
+            <th>product image</th>
+            <th>product name</th>
+            <th>product price</th>
+            <th>action</th>
+         </tr>
+         </thead>
+         <?php while($row = mysqli_fetch_assoc($select)){ ?>
+         <tr>
+            <td><img src="uploaded_img/<?php echo $row['image']; ?>" height="100" alt=""></td>
+            <td><?php echo $row['name']; ?></td>
+            <td>$<?php echo $row['price']; ?>/-</td>
+            <td>
+               <a href="admin_update.php?edit=<?php echo $row['id']; ?>" class="btn"> <i class="fas fa-edit"></i> edit </a>
+               <a href="admin_page.php?delete=<?php echo $row['id']; ?>" class="btn"> <i class="fas fa-trash"></i> delete </a>
+            </td>
+         </tr>
+      <?php } ?>
+      </table>
+   </div>
+
+</div>
+
+
+</body>
+</html>
